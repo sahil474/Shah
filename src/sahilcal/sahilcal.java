c1be43b9b339b51977bfff8dafabb1df572b35b5
admin <sahilloria34@gmail.com>
2020-10-06 02:09:42 +0100

    demo

---
+++ src/sahilcal/sahilcal.java
@@ -0,0 +1,31 @@
+package sahilcal;
+import java.util.Scanner;
+class Main {
+    public static void main(String[] args) {
+
+            System.out.println("hello ");
+            System.out.println("what is the sum of 20+30 ");
+            System.out.println("the sum of  20+30 is given below::");
+
+            int a = 20;
+            int b = 30;
+            System.out.println(a + b);
+
+
+            Scanner loria = new Scanner(System.in);
+            System.out.println("enter num 1 for multiply");
+            int num1 =loria.nextInt();
+            System.out.println("enetr num 2 for multiply");
+            int num2=loria.nextInt();
+            System.out.println("the MULTIPLICATION of entered values is::");
+            System.out.println(num1*num2);
+
+
+
+        }
+
+    }
+
+
+
+
